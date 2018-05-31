ac5762b5a33bffc0c43202e895873a0d1b5a1ddd
Avsanka <38320955+Avsanka@users.noreply.github.com>
2018-05-31 07:26:37 +0000

    Update index.js

--- index.js
+++ index.js
@@ -16,3 +16,22 @@ function minus() {
   result = num1-num2;
   document.getElementById('output').innerHTML = result;
 }
+function multiply() {
+  var num1, num2, result;
+  num1 = document.getElementById('n1').value;
+  num1 = parseInt(num1);
+  num2 = document.getElementById('n2').value;
+  num2 = parseInt(num2);
+  result = num1*num2;
+  document.getElementById('output').innerHTML = result;
+}
+function divide() {
+  var num1, num2, result;
+  num1 = document.getElementById('n1').value;
+  num1 = parseInt(num1);
+  num2 = document.getElementById('n2').value;
+  num2 = parseInt(num2);
+  result = num1:num2;
+  document.getElementById('output').innerHTML = result;
+}
+
