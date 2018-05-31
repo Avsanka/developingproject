90c4affde78d425dc7ea2348b15942c33dd84bcb
Avsanka <38320955+Avsanka@users.noreply.github.com>
2018-05-31 08:12:30 +0000

    Update index.js

--- index.js
+++ index.js
@@ -25,13 +25,3 @@ function multi() {
   result = num1*num2;
   document.getElementById('out').innerHTML = result;
 }
-
-function divide() {
-  var num1, num2, result;
-  num1 = document.getElementById('n1').value;
-  num1 = parseInt(num1);
-  num2 = document.getElementById('n2').value;
-  num2 = parseInt(num2);
-  result = num1:num2;
-  document.getElementById('out').innerHTML = result;
-}
